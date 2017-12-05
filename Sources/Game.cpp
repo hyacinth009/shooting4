996e1e316aec17d0edfb4a4f81dbdc44a5890e49
hw16a146 <hw16a146@oecu.jp>
2017-12-05 15:51:01 +0900

    Hの作業完了

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -34,6 +34,7 @@ void Update()
     // 弾の発射
     if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
         bulletPos = cannonPos + Vector2(50, 10);
+        PlaySound("se_maoudamashii_explosion03.mp3");
     }
 
     // 弾の移動
@@ -45,6 +46,8 @@ void Update()
         if (targetRect.Overlaps(bulletRect)) {
             score += 1;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
+            
+            PlaySound("se_maoudamashii_system27.mp3");//SEを再生する（実装：HW16A146　長杉星弥）
         }
     }
 
